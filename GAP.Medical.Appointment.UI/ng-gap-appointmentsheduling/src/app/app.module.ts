6159ec82cd2d04783fcd5093f15db0aa2d400716
JulianToro <Julian@gmail.com>
2020-01-26 09:43:56 -0500

    Se agregan componentes

--- GAP.Medical.Appointment.UI/ng-gap-appointmentsheduling/src/app/app.module.ts
+++ GAP.Medical.Appointment.UI/ng-gap-appointmentsheduling/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { MainComponent } from './components/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PatientComponent } from './components/patient/patient.component';
+import { SheduleComponent } from './components/shedule/shedule.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AppointmentComponent,
+    MainComponent,
+    MenuComponent,
+    PatientComponent,
+    SheduleComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
